a0a1ca93ea3d78c2d80c9295038243c4c30cf6cb
Bailey <bapcos@gmail.com>
2020-09-29 01:48:52 -0400

    Client Project

---
+++ TicTacToe_Client/src/main/java/com/tictactoe_client/App.java
@@ -0,0 +1,21 @@
+package com.tictactoe_client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ TicTacToe_Client/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.tictactoe_client {
+    requires javafx.controls;
+    exports com.tictactoe_client;
+}
