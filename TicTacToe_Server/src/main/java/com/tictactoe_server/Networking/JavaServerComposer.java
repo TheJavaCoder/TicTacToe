53284bab7fef90e4bec4d7b2fabdcaee38c15570
Bailey <bapcos@gmail.com>
2020-10-08 23:02:13 -0400

    Clients Connected to the server

---
+++ TicTacToe_Server/src/main/java/com/tictactoe_server/Networking/JavaServerComposer.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tictactoe_client.Networking;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ *
+ * @author bapco
+ */
+public class JavaServerComposer implements INetworkComposer{
+
+    
+    @Override
+    public String sendGameHistoryData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String sendMoveData(Move m) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String sendIllegalMove() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String sendGameReadyState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String sendGameFinishedState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String sendUsername() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void compose(String s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object createConnection(String server, int port) {
+        
+        try {
+            Socket connection = new Socket(server, port);
+            
+            return connection;
+        } catch (IOException ex) {
+        
+            return null;
+        }
+    }
+    
+}
---
+++ TicTacToe_Server/src/main/java/com/tictactoe_server/Networking/JavaServerParser.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tictactoe_client.Networking;
+
+
+/**
+ *
+ * @author bapco
+ */
+public class JavaServerParser implements INetworkParser{
+
+    @Override
+    public void parse(String message) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Move getMove(String message) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
